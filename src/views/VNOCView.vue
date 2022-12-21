2da21f50f40d4fb4c430d1a09f3664e5486121e2
Ellery Hulme <elleryh@insightsystems.com.au>
2022-12-22 09:43:19 +1100

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   src/views/VNOCView.vue

--- src/views/VNOCView.vue
+++ src/views/VNOCView.vue
@@ -3,7 +3,7 @@
     <header class="container">
       <h1 class="">Video Wall</h1>
       <div
-        @click="VWpower = !VWpower"
+        @click="mainPwwr"
         class="btn-rnd"
         :class="{ true: VWpower, false: !VWpower }">
         <font-awesome-icon
@@ -11,14 +11,21 @@
           size="2x" />
       </div>
     </header>
-    <div class="container controls">
+    <div class="controls">
       <section
         id="LCD"
         class="card">
-        <h1 class="card-title">Screen 1 control</h1>
+        <h1 class="card-title">Screen {{ screenCon }} control</h1>
         <div class="btn-con">
           <h3>Screen Power:</h3>
-          <div class="btn-rnd btn-rnd-sm">
+          <div
+            @click="lcdsPwr(lcds[screenCon - 1])"
+            id="con-pwr"
+            class="btn-rnd btn-rnd-sm"
+            :class="{
+              true: lcds[screenCon - 1].on,
+              false: !lcds[screenCon - 1].on,
+            }">
             <font-awesome-icon icon="fa-solid fa-power-off" />
           </div>
         </div>
@@ -27,7 +34,8 @@
           <div class="select">
             <select
               name="input"
-              id="input">
+              id="input"
+              v-model="lcds[screenCon - 1].input">
               <option value="1">HDMI 1</option>
               <option value="2">HDMI 2</option>
               <option value="3">RGBHV</option>
@@ -36,15 +44,71 @@
           </div>
         </div>
       </section>
+      <section class="screens">
+        <div
+          v-for="LCD in lcds"
+          :key="LCD.id"
+          @click="screenCon = LCD.id"
+          class="lcd"
+          :class="{ active: screenCon == LCD.id }">
+          <h2>{{ LCD.id }}</h2>
+          <font-awesome-icon
+            class="LCD-pwr"
+            :class="{ on: LCD.on }"
+            icon="fa-solid fa-power-off" />
+        </div>
+      </section>
     </div>
-    
   </main>
 </template>
 
 <script setup>
   import { ref } from '@vue/reactivity'
 
-  const VWpower = ref(true)
+  const VWpower = ref(true),
+    screenCon = ref(1),
+    lcds = ref([
+      {
+        id: 1,
+        on: false,
+        input: 4,
+      },
+      {
+        id: 2,
+        on: true,
+        input: 1,
+      },
+      {
+        id: 3,
+        on: true,
+        input: 1,
+      },
+      {
+        id: 4,
+        on: false,
+        input: 3,
+      },
+      {
+        id: 5,
+        on: true,
+        input: 1,
+      },
+      {
+        id: 6,
+        on: true,
+        input: 1,
+      },
+    ]),
+    mainPwwr = () => {
+      VWpower.value = !VWpower.value
+      lcds.value.forEach(e => {
+        e.on = VWpower.value
+      })
+    },
+    lcdsPwr = e => {
+      e.on = !e.on
+      if (e.on) VWpower.value = true
+    }
 </script>
 
 <style scoped>
@@ -60,14 +124,49 @@
   .controls {
     height: 75%;
     width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    flex-direction: column-reverse;
+  }
+  .screens {
+    margin-left: 0.5em;
+    /* height: 75%; */
+    width: 50%;
+
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(2, minmax(150px 200px));
+  }
+  .lcd {
+    width: 100%;
+    aspect-ratio: 16/9;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: solid var(--light3);
+  }
+  .lcd > h2 {
+    margin-left: auto;
+  }
+  .lcd > svg {
+    margin: auto 1em 1em auto;
+  }
+  .LCD-pwr {
+    color: var(--fail);
+  }
+  .LCD-pwr.on {
+    color: var(--success);
   }
   #LCD {
     min-height: 250px;
     /* width: 100%; */
   }
+  #con-pwr.true,
   .true {
     background: linear-gradient(145deg, #62be78, #53a065);
   }
+  #con-pwr.false,
   .false {
     background: linear-gradient(145deg, #ff7878, #e66565);
   }
@@ -114,5 +213,9 @@
   .select:after {
     grid-area: select;
     justify-self: end;
+  }
+  .active {
+    color: var(--gold);
+    border-color: var(--gold);
   }
 </style>
