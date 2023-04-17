6f7c2e6f2ae8ba6ad1f4d3a4e5478d007637c17a
Ellery <elleryh@insightsystems.com.au>
2023-04-17 21:55:40 +1000

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	new file:   public/Icon.png 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,29 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+import { type } from 'node:os'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        icons: [
+          {
+            src: 'Icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
